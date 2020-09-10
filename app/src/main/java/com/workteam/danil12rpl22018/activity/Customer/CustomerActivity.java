658840dbd4c6b7664f6a61397faecb507a55b4d1
ahmadzaky18 <ahmadzakyxrpl2018smkrus@gmail.com>
2020-09-10 14:56:30 +0700

    update

---
+++ app/src/main/java/com/workteam/danil12rpl22018/activity/Customer/CustomerActivity.java
@@ -0,0 +1,16 @@
+package com.workteam.danil12rpl22018.activity.Customer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.workteam.danil12rpl22018.R;
+
+public class CustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer);
+    }
+}
